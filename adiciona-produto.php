ad86ddf4637e983bf894d109b3f0f1cca3b5a9c2
Otto Werner <ottowlc@gmail.com>
2021-08-10 18:14:07 -0300

    Update
    Adicionado a função include para reaproveitamento do código

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Loja</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/loja.css">
-</head>
-<body>
+<?php include("cabecalho.php") ?>
 
-	<div class="container">
-		<div class="principal">
 			<?php
 
 			$nome = $_GET['nome'];
@@ -20,8 +10,5 @@
 			<p class="alert-success">
 				Produto <?php echo $nome; ?>, <?php echo $preco; ?> adicionado com sucesso!!!
 			</p>
-		</div>
-	</div>
-
-</body>
-</html>
+			
+<?php include("rodape.php") ?>	
--- produto-formulario.php
+++ produto-formulario.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Loja</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/loja.css">
-</head>
-<body>
-
-	<div class="container">
-		<div class="principal">
+<?php include("cabecalho.php") ?>
 
 			<h1>Formulário de Produto</h1>
 
@@ -26,8 +15,5 @@
 				<input type="submit" value="cadastrar">
 
 			</form>
-		</div>
-	</div>
-
-</body>
-</html>
+			
+<?php include("rodape.php") ?>	
