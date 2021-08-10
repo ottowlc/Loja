46afc13bbc9f0a1b79ab033f175a3ca1fb60bf27
Otto Werner <ottowlc@gmail.com>
2021-08-10 17:43:22 -0300

    Update
    Adicionador formulário para cadastrar o produto

---
+++ produto-formulario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<form action="adiciona-produto.php">
+		
+		Nome:
+		<input type="text" name="nome">
+		<br/>
+
+		Preço
+		<input type="number" name="preco">
+		<br/>
+
+		<input type="submit" value="cadastrar">
+
+	</form>
+
+</body>
+</html>
