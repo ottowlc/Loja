bf398f4fad6dd298bee22760002c3202f6c4dc22
Otto Werner <ottowlc@gmail.com>
2021-08-10 18:12:04 -0300

    Update
    Adicionado a função include chamando os arquivos cabecalho.php e rodape.php

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Loja</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/loja.css">
-</head>
-<body>
+<?php include("cabecalho.php") ?>
 
-	<div class="container">
-		<div class="principal">
 			<h1>Bem vindo!</h1>
-		</div>
-	</div>
 
-</body>
-</html>
+<?php include("rodape.php") ?>	
