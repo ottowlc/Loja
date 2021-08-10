63f4b45e6be770e4f8fc4518b90f4d26479a7764
Otto Werner <ottowlc@gmail.com>
2021-08-10 17:51:15 -0300

    Update
    Adicionado css externo

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>Loja</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/loja.css">
 </head>
 <body>
 
