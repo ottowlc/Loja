400f96f4c74a2363b6530e2776b6910cf986a21d
Otto Werner <ottowlc@gmail.com>
2021-08-18 18:09:55 -0300

    Update
    Criado a listagem de produto

--- produto-lista.php
+++ produto-lista.php
@@ -1,4 +1,14 @@
 <?php include("cabecalho.php"); ?>
 <?php include("conecta.php"); ?>
 
+<?php  
+
+	$resultado = mysqli_query($conexao, "select * from produtos");
+
+	while($produto =  mysqli_fetch_assoc($resultado)) {
+		echo $produto['nome'] . "<br/>";
+	}
+
+?>
+
 <?php include("cabecalho.php"); ?>
